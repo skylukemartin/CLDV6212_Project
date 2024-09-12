d5dd76cd402f542bc5e7c37a3edd3328660967a7
Sky Martin <131005117+skylukemartin@users.noreply.github.com>
2024-09-12 09:18:45 +0200

    Create initial functions project

---
+++ WebAppFuncs/AzureFuncsRoot.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace WebAppFuncs.Function
+{
+    public class AzureFuncsRoot
+    {
+        private readonly ILogger<AzureFuncsRoot> _logger;
+
+        public AzureFuncsRoot(ILogger<AzureFuncsRoot> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("AzureFuncsRoot")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
